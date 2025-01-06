59743aedfd3f0082aa9e7a623853b2ba1b938d20
arniar <developerarjun6@gmail.com>
2025-01-06 16:30:55 +0530

    ongoing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var kidRouter = require('./kid');
+var womenRouter = require('./women');
+var menRouter = require('./men');
+var shopRouter = require('./shop');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home');
 });
+
+router.use('/kid',kidRouter);
+router.use('/women',womenRouter);
+router.use('/men',menRouter);
+router.use('/shop',shopRouter);
+
 
 module.exports = router;
